dfb9419e70f03d51b2bb61457b0b5ef36b76824a
Damian <froispro6969@gmail.com>
2026-03-24 21:07:07 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Witaj w IT Solution!
+      </h1>
+    </div>
+  )
+}
